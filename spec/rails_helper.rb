8a91f26c0ae0b97158f5a8b81e588214b47469f6
slague <stephanielague@gmail.com>
2017-06-01 16:51:40 -0600

    Updates public folder views
    * Public folders and binaries available to non-logged in visitors 
    * Update public folders controller
    * Update routes (un-nested public folders from users)
    * Updated corresponding routes and views
    * Added user_with_public_folders factory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,11 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
+
 include ActionDispatch::TestProcess
 # Add additional requires below this line. Rails is not loaded until this point!
 
