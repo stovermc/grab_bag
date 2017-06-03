8b88509b9f40f357ed0ffb5fc2ffca4c553c81c7
Seth <seth19483@gmail.com>
2017-06-03 11:54:52 -0600

    created seed data

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20170531210008) do
     t.datetime "updated_at",             null: false
     t.integer  "permission", default: 0
     t.string   "slug"
-    t.string   "iv"
     t.integer  "status",     default: 0
     t.index ["folder_id"], name: "index_folders_on_folder_id", using: :btree
     t.index ["slug"], name: "index_folders_on_slug", using: :btree
@@ -79,12 +78,12 @@ ActiveRecord::Schema.define(version: 20170531210008) do
     t.string   "email"
     t.string   "phone"
     t.string   "token"
-    t.datetime "created_at",                                                                                null: false
-    t.datetime "updated_at",                                                                                null: false
+    t.datetime "created_at",                                                                                        null: false
+    t.datetime "updated_at",                                                                                        null: false
     t.string   "password_digest"
     t.integer  "role",              default: 0
     t.string   "verification_code"
-    t.string   "avatar_url",        default: "https://robohash.org/aaccusamusab.png?size=300x300&set=set1"
+    t.string   "avatar_url",        default: "https://robohash.org/quimolestiasmollitia.png?size=300x300&set=set1"
   end
 
   add_foreign_key "binaries", "folders"
