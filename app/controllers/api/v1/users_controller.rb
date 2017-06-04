d3e8ad4dcdf52672963f4e6ef391491fa06723be
stovermc <stovermc@gmail.com>
2017-06-04 12:07:28 -0600

    Add current user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :doorkeeper_authorize!
 
   def index
-    render json: User.all
+    render json: current_user.owned_folders
   end
 
+  private
+    def current_user
+      @current_user = User.find(doorkeeper_token.resource_owner_id)
+    end
 end
