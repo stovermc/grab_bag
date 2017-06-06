5e8795e5cd2777550958795500225af6b513155b
Victoria Vasys <victoria.vasys@gmail.com>
2017-06-06 14:18:05 -0600

    Update user_request_spec.rb

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -26,30 +26,5 @@ describe "Users API" do
     expect(users.first).to_not have_key("password")
     expect(users.first).to_not have_key("password_diget")
   end
-  
-  it "sends a list of users" do
-    2.times do |n|
-      User.create!(name: "name#{n}",
-                  username: "username#{n}",
-                  email: "email#{n}@grabbag.com",
-                  phone: '5555555555',
-                  status: 'active',
-                  password: 'banana',
-                  avatar_url: "https://robohash.org/#{n}")
-    end
 
-    get '/api/v1/users'
-
-    expect(response).to be_success
-
-    users = JSON.parse(response.body)
-
-    expect(users.count).to eq(2)
-    expect(users.first).to have_key("name")
-    expect(users.first).to have_key("username")
-    expect(users.first).to have_key("email")
-    expect(users.first).to have_key("phone")
-    expect(users.first).to_not have_key("password")
-    expect(users.first).to_not have_key("password_diget")
-  end
 end
