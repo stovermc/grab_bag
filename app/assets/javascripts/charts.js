8c7ad120881ff40742557d08614f73d097904f2f
VictoriaVasys <vvasys@gmail.com>
2017-06-05 19:40:43 -0600

    Add binaries by type endpoints & start chart

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -62,5 +62,39 @@ function drawBinaryDownloadsPubPrivPlot(data) {
    .text("Public vs Private Binary Downloads");
 }
 
+var loadBinariesByTypeData() = function(){
+                $.ajax({
+                  type: 'GET',
+                  contentType: 'application/json; charset=utf-8',
+                  url: '/api/v1/binaries_by_type',
+                  dataType: 'json',
+                  success: function(data){
+                    drawBinariesByTypePlot(data);
+                  },
+                  failure: function(result){
+                    error();
+                  }
+                });
+              };
+              
+// function drawBinariesByTypePlot(data) {
+// 
+//   var svg = dimple.newSvg('#binaries_by_type_plot', "100%", "100%");
+//   var chart = new dimple.chart(svg, data);
+//   chart.addMeasureAxis("p", "total");
+//   chart.addSeries('file_type', dimple.plot.pie);
+//   chart.addLegend(450, 20, 90, 300, "left");
+//   chart.draw();
+//   svg.append("text")
+//    .attr("x", 295)
+//    .attr("y", 30)
+//    .attr("text-anchor", 'middle')
+//    .style("font-size", "30px")
+//    .style("font-family", "sans-serif")
+//    .style("font-weight", "bold")
+//    .text("Binaries By File Type");
+// }
+
 $('#binary_downloads_by_date_plot').append(loadBinaryDownloadsData())
 $('#binary_downloads_public_v_private_plot').append(loadBinaryDownloadsPubPrivData())
+$('#binaries_by_type_plot').append(loadBinariesByTypeData())
